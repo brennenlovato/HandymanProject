d2bc00f8b07c0076ad6d8840302452e4cf97ab90
Brennen Lovato <brennenlovato@outlook.com>
2022-09-15 19:04:31 -0600

    added controller template

--- app/controllers/api/services_controller.rb
+++ app/controllers/api/services_controller.rb
@@ -1,2 +1,37 @@
 class Api::ServicesController < ApplicationController
+  def index
+    render json: Model_name.all
+  end
+
+  def show
+    @model_name = Model_name.find(params[:id])
+    render json: @model_name
+  end
+
+  def create
+    @model_name = Model_name.new(model_name_params)
+    if @model_name.save
+      render json: @model_name
+    else
+      render json: { errors: @model_name.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @model_name = Model_name.find(params[:id])
+    if @model_name.update(model_name_params)
+      render json: @model_name
+    else
+      render json: { errors: @model_name.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @model_name = Model_name.find(params[:id])
+    @model_name.destroy
+    render json: { message: 'model_name deleted' }
+    or
+    Model_name.find(params[:id]).destroy
+    render json: { message: 'model_name deleted' }
+  end
 end
